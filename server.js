2026264025ba37fe62b47fd29479099f675764a8
Esther Carrasco <silviacarrascoescobar@gmail.com>
2024-02-03 18:12:39 -0700

    fourth commit (assignment 4)

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app
     .use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
       })
     .use('/', require('./routes'));
